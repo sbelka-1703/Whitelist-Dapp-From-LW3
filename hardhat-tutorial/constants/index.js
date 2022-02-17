9524849297fd161a2154cadc190ebf7a18c7bfb5
sbelka-1703 <sbelka93@gmail.com>
2022-02-17 08:24:36 -0700

    new SC address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x87655AEBc2F405eA277ed7134Ad4B0040727a7AD";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-lw3-sbelka-1703.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-lw3.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
